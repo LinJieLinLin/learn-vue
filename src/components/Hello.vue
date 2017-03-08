4f75cae86e55806e0731d18352d05be091562754
linjielinlin <993353454@qq.com>
2017-03-09 00:59:35 +0800

    use scss

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -30,10 +30,12 @@ export default {
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+$cRed:red;
 h1,
 h2 {
     font-weight: normal;
+    color: $cRed;
 }
 
 ul {
